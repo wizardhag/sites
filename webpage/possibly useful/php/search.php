d6b687abac0d8f46ef9ff8fabf85bc981d9b9ac7
wizardhag <lescienceshow2@gmail.com>
2016-09-16 17:17:45 -0400

    Update search.php

--- webpage/possibly useful/php/search.php
+++ webpage/possibly useful/php/search.php
@@ -2,5 +2,6 @@
 
 $query = searchBox;
 
-echo "The search function is not enabled yet (I don't know how to use php)";
+echo "The search function is not enabled yet (I don't know how to use php)<br>";
+echo "<a href='../illuminatiSite.html'><--Back to home page</a>";
 ?>
